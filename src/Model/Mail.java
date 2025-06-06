209cd67559f962c03b2b12814af8ec0eaa6ef866
TomyBermejo <131066317+TomyBermejo@users.noreply.github.com>
2025-06-06 11:01:17 -0300

    Update Mail.java

--- src/Model/Mail.java
+++ src/Model/Mail.java
@@ -1,5 +1,7 @@
 package Model;
 
 public class Mail {
-
+    public void enviarCorreo(String correo, String contenido) {
+        System.out.println("Correo enviado a " + correo + ": " + contenido);
+    }
 }

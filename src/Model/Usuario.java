df041f081d7c200d88c05c88c63720f0a0660450
Franco Baltasar Massi <francomassi1@gmail.com>
2025-06-15 16:30:49 -0300

    Update Usuario.java

--- src/Model/Usuario.java
+++ src/Model/Usuario.java
@@ -11,6 +11,7 @@ public class Usuario {
 	private List<UsuarioDeporte> deportes;
 	private Ubicacion ubicacion;
 	private List<Partido> partidos;
+	private boolean sesion;
 
 	public Usuario(String nombreUsuario, String correo, String password) {
 		this.nombreUsuario = nombreUsuario;
@@ -23,24 +24,54 @@ public class Usuario {
 		System.out.println("Usuario registrado: " + this.nombreUsuario);
 	}
 
-	public List<Partido> consultarHistorialPartidos() {
-		return partidos;
+	public List<PartidoDTO> consultarHistorialPartidos() {
+        	return partidos.stream().map(Partido::toDTO).collect(Collectors.toList());
 	}
 
-	public Partido buscarPartido(Ubicacion ubicacionDeseada) {
+	public PartidoDTO buscarPartido(Ubicacion ubicacionDeseada) {
+        	
 		Partido partidoCercano = null;
-		double distanciaMinima = Double.MAX_VALUE;
-		for (Partido partido : partidos) {
-			double distancia = partido.getUbicacion().calcularDistancia(ubicacionDeseada);
-			if (distancia < distanciaMinima) {
+        	double distanciaMinima = Double.MAX_VALUE;
+
+        	for (Partido partido : partidos) {
+           		double distancia = partido.getUbicacion().calcularDistancia(ubicacionDeseada);
+		   	if (distancia < distanciaMinima) {
 				distanciaMinima = distancia;
 				partidoCercano = partido;
-			}
-		}
-		return partidoCercano;
-	}
+		    	}
+        	}
 
-	public boolean login(String nombreUsuario, String password) {
-        return nombreUsuario.equals(this.nombreUsuario) && password.equals(this.password);
-	}
+        	return partidoCercano != null ? partidoCercano.toDTO() : null;
+    	}	
+
+	public UsuarioDTO login(String nombreUsuario, String password) {
+			if (nombreUsuario.equals(this.nombreUsuario) && password.equals(this.password)) {
+			    this.sesion = true;
+			    System.out.println("Login exitoso para el usuario: " + nombreUsuario);
+			    return this.toDTO();
+			}
+        	return null;
+	   }
+
+	public UsuarioDTO logout() {
+		if (this.sesion.equals(true)) {
+			this.sesion = false;
+			System.out.println("Sesion cerrada: " + nombreUsuario);
+			return this.toDTO();
+		}
+        	return null;
+	   }
+
+	public UsuarioDTO toDTO() {
+        	UsuarioDTO dto = new UsuarioDTO();
+        	dto.setNombreUsuario(this.nombreUsuario);
+        	dto.setUbicacion(this.ubicacion != null ? this.ubicacion.toDTO() : null);
+		dto.setDeportes(this.deportes != null ? this.deportes.stream()
+                	.map(UsuarioDeporte::toDTO)
+                	.collect(Collectors.toList()) : null);
+       		dto.setHistorial(this.partidos != null ? this.partidos.stream()
+                	.map(Partido::toDTO)
+                	.collect(Collectors.toList()) : null);
+        	return dto;
+    }
 }
